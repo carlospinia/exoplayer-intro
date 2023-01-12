dae53dc292870e1aa621847f90cecd22a1f48d52
Carlos Piña <carlos.p.m.2293@gmail.com>
2023-01-12 12:41:31 +0100

    Test exoplayer notification

--- exoplayer-codelab-00/src/main/java/com/example/exoplayer/PlayerActivity.kt
+++ exoplayer-codelab-00/src/main/java/com/example/exoplayer/PlayerActivity.kt
@@ -15,9 +15,24 @@
  */
 package com.example.exoplayer
 
+import android.app.NotificationManager.IMPORTANCE_HIGH
+import android.app.PendingIntent
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.media3.common.MediaItem
+import androidx.media3.common.Player
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.exoplayer.trackselection.DefaultTrackSelector
+import androidx.media3.exoplayer.trackselection.TrackSelector
+import androidx.media3.exoplayer.upstream.BandwidthMeter
+import androidx.media3.exoplayer.upstream.DefaultBandwidthMeter
+import androidx.media3.ui.PlayerNotificationManager
 import com.example.exoplayer.databinding.ActivityPlayerBinding
+
 
 /**
  * A fullscreen activity to play audio or video streams.
@@ -28,8 +43,74 @@ class PlayerActivity : AppCompatActivity() {
         ActivityPlayerBinding.inflate(layoutInflater)
     }
 
+    private var exoPlayer: ExoPlayer? = null
+    private var playerNotificationManager: PlayerNotificationManager? = null
+    private val notificationId = 1234
+
+    private val mediaDescriptionAdapter: PlayerNotificationManager.MediaDescriptionAdapter =
+        object : PlayerNotificationManager.MediaDescriptionAdapter {
+            override fun getCurrentContentTitle(player: Player): CharSequence =
+                "Prueba reproductor de audio"
+
+            override fun createCurrentContentIntent(player: Player): PendingIntent {
+                val intent = Intent(this@PlayerActivity, PlayerActivity::class.java)
+                val flags = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+                } else {
+                    PendingIntent.FLAG_UPDATE_CURRENT
+                }
+                return PendingIntent.getActivity(
+                    this@PlayerActivity,
+                    (System.currentTimeMillis() / 1000).toInt(),
+                    intent,
+                    flags
+                )
+            }
+
+            override fun getCurrentContentText(player: Player): CharSequence =
+                "Este reproductor de audio funciona en segundo plano"
+
+            override fun getCurrentLargeIcon(
+                player: Player,
+                callback: PlayerNotificationManager.BitmapCallback
+            ): Bitmap = BitmapFactory.decodeResource(resources, R.mipmap.ic_launcher)
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(viewBinding.root)
+
+        val bandwidthMeter: BandwidthMeter = DefaultBandwidthMeter.Builder(this).build()
+        val trackSelector: TrackSelector = DefaultTrackSelector(this)
+
+        val exoPlayer = ExoPlayer.Builder(this)
+            .setTrackSelector(trackSelector)
+            .setBandwidthMeter(bandwidthMeter)
+            .build()
+        viewBinding.playerView.player = exoPlayer
+
+        val mediaItem = MediaItem.fromUri(getString(R.string.media_url_mp3))
+        exoPlayer.setMediaItem(mediaItem)
+
+        playerNotificationManager =
+            PlayerNotificationManager.Builder(this, notificationId, "AUDIO_CHANNEL_ID")
+                .setChannelNameResourceId(R.string.app_name)
+                .apply {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                        setChannelImportance(IMPORTANCE_HIGH)
+                    }
+                }
+                .setMediaDescriptionAdapter(mediaDescriptionAdapter).build()
+        playerNotificationManager?.setPlayer(exoPlayer)
+        exoPlayer.prepare()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        playerNotificationManager?.setPlayer(null)
+        exoPlayer?.let {
+            it.release()
+            exoPlayer = null
+        }
     }
 }
